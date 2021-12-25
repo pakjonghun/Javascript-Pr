4bad00785b9c6a0c6d03ddab5682e7e630172441
정훈박 <fireking5997@gmail.com>
2021-12-26 00:15:18 +0900

    Add:dimple modal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import "./style.css";
+const modal = document.querySelector(".modal");
+const showModal = document.querySelectorAll(".show-modal");
+const closeModal = document.querySelector(".close-modal");
+const overlay = document.querySelector(".overlay");
 
-console.log(1);
+showModal.forEach((item) =>
+  item.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+    overlay.classList.toggle("hidden");
+  })
+);
+
+closeModal.addEventListener("click", () => {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+});
